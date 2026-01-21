f98b895860f439438532ce755a7d68a4d1ccee36
Xnaizer <seykig028@gmail.com>
2026-01-21 14:11:26 +0700

    Mark soal4.js as not done yet

--- 00 - Phase 0/Week3-Object-is-a-key/quiz/part1/soal4.js
+++ 00 - Phase 0/Week3-Object-is-a-key/quiz/part1/soal4.js
@@ -37,3 +37,7 @@ console.log(cariModus([10, 4, 5, 2, 4])); // 4
 // console.log(cariModus([10, 3, 1, 2, 5])); // -1
 // console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
 // console.log(cariModus([7, 7, 7, 7, 7])); // -1
+
+
+
+// not done yet
